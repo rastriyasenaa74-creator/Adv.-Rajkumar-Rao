195942aed8de1ef97d614878de2e77c3c37a9115
rastriyasenaa74-creator <rastriyasenaa74@gmail.com>
2026-09-17 16:46:16 +0530

    Add files via upload

---
+++ Assets/config.js
@@ -0,0 +1,9 @@
+const SITE_CONFIG = {
+  API_URL: "YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL",
+  ADMIN_EMAIL: "advocaterajkumarrao@gmail.com",
+  PHONE: "+918810904208",
+  WHATSAPP: "+917348331408",
+  EMAIL: "advocaterajkumarrao@gmail.com",
+  ADVOCATE: "Advocate Rajkumar Rao",
+  COURT: "District Court, Mirzapur, Uttar Pradesh"
+};
