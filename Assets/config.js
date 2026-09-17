326c0774940e8af9b7f5ae3e6e271ef4f0ad264b
rastriyasenaa74-creator <rastriyasenaa74@gmail.com>
2026-09-17 17:07:45 +0530

    Delete Assets directory

--- Assets/config.js
+++
@@ -1,9 +0,0 @@
-const SITE_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxgbOyOxRmBk26vU_oJk4xqHKvyMJn3ChV0koBHV3Y6UbC0JQ9QtvEUir1nT1ojfYI/exec",
-  ADMIN_EMAIL: "advocaterajkumarrao@gmail.com",
-  PHONE: "+918810904208",
-  WHATSAPP: "+917348331408",
-  EMAIL: "advocaterajkumarrao@gmail.com",
-  ADVOCATE: "Advocate Rajkumar Rao",
-  COURT: "District Court, Mirzapur, Uttar Pradesh"
-};
